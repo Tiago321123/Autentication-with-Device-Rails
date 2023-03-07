70cef98f1e3bf97526e335e1a246ada32be901d4
Tiago321123 <tiagodesouza12310@gmail.com>
2023-03-06 22:11:45 -0300

    Devise completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
-end
+    # O padrão de atributos permitidos no Devise são os campos email, senha e confirmação de senha. Então, quando o desenvolvedor quer permitir que o usuário crie, por exemplo, seu nome ou escolha um perfil de acesso para o sistema, é preciso configurar a permissão desses parâmetros.
+    before_action :configure_permitted_parameters, if: :devise_controller?
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
+    end
+    # A partir de agora, o Devise irá autorizar o registro de nome na criação do usuário (“:sign_up”), mas vale lembrar que é preciso configurar também caso seja permitido editar o nome, que utilizaria o “:account_update”.
